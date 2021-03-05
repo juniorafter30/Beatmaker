876c3a79f952cdca41e2dd640edec750b1f31a43
juniorafter30 <75785576+juniorafter30@users.noreply.github.com>
2021-03-05 18:40:32 +0100

    starting with JS

---
+++ app.js
@@ -0,0 +1,47 @@
+class DrumKit {
+     constructor(){
+         this.pads = document.querySelectorAll('.pad');
+         this.playBtn = document.querySelector('.play');
+         this.kickAudio = document.querySelector('.kick-sound');
+         this.snareAudio = document.querySelector('.snare-sound');
+         this.clapAudio = document.querySelector('.clap-sound');
+         this.index = 0;
+         this.bpm = 150;
+     }
+
+     activePad(){
+         this.classList.toggle("active");
+     }
+     
+     repeat(){
+         let step = this.index % 8;
+         const activeBars = document.querySelectorAll(`.b${step}`)
+         this.index++;
+         activeBars.forEach(bar => {
+             bar.style.animation = `playTrack 0.3s alternate ease-in-out`
+         })
+     }
+
+     start(){
+         const interval = (60/this.bpm) * 1000;
+
+     setInterval (() => {
+             this.repeat();
+         }, interval)
+
+     }
+};
+
+const drumKit = new DrumKit();
+
+drumKit.pads.forEach(pad => {
+    pad.addEventListener('click', drumKit.activePad);
+    pad.addEventListener('animationend', function(){
+        this.style.animation = ""; 
+    });
+
+});
+drumKit.playBtn.addEventListener('click', function (){
+    drumKit.start();
+});
+
